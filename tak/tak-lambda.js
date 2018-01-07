c862a39ad3f63d101b877537c7e91ad35ff8e489
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-07 12:22:01 +0530

    s3-put-on-s3-put

---
+++ tak/tak-lambda.js
@@ -0,0 +1,23 @@
+let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+exports.handler = function (event, context, callback) {
+    s3.putObject({
+        "Body": `${event}`,
+        "Bucket": "s3-lambda-s3-data",
+        "Key": "event.json"
+    })
+        .promise()
+        .then(data => {
+            console.log(data);           // successful response
+            /*
+            data = {
+                ETag: "\\"6805f2cfc46c0f04559748bb039d69ae\\"", 
+                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
+            }
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
+
+}
