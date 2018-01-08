f3797e090014c27897624d635b94c5345975ee3a
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-08 15:11:12 +0530

    kitty

--- tak/tak-lambda.js
+++ tak/tak-lambda.js
@@ -1,4 +1,23 @@
 let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 exports.handler = function(event, context, callback){
+s3.putObject({
+  "Body": e,
+  "Bucket": "apig-kine-proxy",
+  "Key": "event.json"
+})
+    .promise()
+    .then(data => {
+        console.log(data);           // successful response
+        /*
+        data = {
+            ETag: "\\"6805f2cfc46c0f04559748bb039d69ae\\"", 
+            VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
+        }
+        */
+    })
+    .catch(err => {
+        console.log(err, err.stack); // an error occurred
+    });
 
 }
