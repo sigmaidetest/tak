91784795aa5ed9911d691d9850a367883401a049
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-08 11:57:27 +0530

    s3-console

--- tak/tak-lambda.js
+++ tak/tak-lambda.js
@@ -1,22 +1,4 @@
 let AWS = require('aws-sdk');
-const s3 = new AWS.S3();
-exports.handler = function (event, context, callback) {
-    s3.putObject({
-        "Body": "{}",
-        "Bucket": "s3-lambda-s3-data",
-        "Key": "event.json"
-    })
-        .promise()
-        .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                ETag: "\\"6805f2cfc46c0f04559748bb039d69ae\\"", 
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-            }
-            */
-        })
-        .catch(err => {
-            console.log(err, err.stack); // an error occurred
-        });
+exports.handler = function(event, context, callback){
+    console.log("balla")
 }
