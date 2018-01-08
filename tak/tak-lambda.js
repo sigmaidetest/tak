78cf7f97b2bfa5f0b489323bff196fdd5301d281
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-08 12:52:14 +0530

    s3-log-all-to-console

--- tak/tak-lambda.js
+++ tak/tak-lambda.js
@@ -1,4 +1,6 @@
 let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback){
-    console.log(event);
+    console.log(JSON.stringify(event));
+    console.log(JSON.stringify(context));
+    console.log(JSON.stringify(callback));
 }
