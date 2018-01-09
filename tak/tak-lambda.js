19387cb449979e884f02bf2b840f1398e3aff9bb
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-09 18:19:44 +0530

    teng

--- tak/tak-lambda.js
+++ tak/tak-lambda.js
@@ -2,7 +2,7 @@ let AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
     s3.listObjects({
-        'Bucket': 's3-lambda-s3',
+        'Bucket': 's3-lambda-s3-data',
         'MaxKeys': 10,
         'Prefix': ''
     }).promise()
