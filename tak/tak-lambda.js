baf4a3e50b80db4e41aeab96be90c49fcad98cba
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-10 17:04:28 +0530

    tikka

--- tak/tak-lambda.js
+++ tak/tak-lambda.js
@@ -1,12 +1,15 @@
 let AWS = require('aws-sdk');
+const sqs = new AWS.SQS();
+const s3 = new AWS.S3();
+const ddb = new AWS.DynamoDB.DocumentClient();
 const sns = new AWS.SNS();
 exports.handler = function (event, context, callback) {
     sns.publish({
-        Message: 'tik',
-        Subject: 'tak',
+        Message: 'd',
+        Subject: 'c',
         MessageAttributes: {},
         MessageStructure: 'String',
-        TopicArn: 'arn:aws:sns:us-east-1:359675929438:tak'
+        TopicArn: 'arn:aws:sns:us-east-1:359675929438:a'
     }).promise()
         .then(data => {
             // your code goes here
@@ -14,4 +17,49 @@ exports.handler = function (event, context, callback) {
         .catch(err => {
             // error handling goes here
         });
+    ddb.put({
+        TableName: 'a',
+        Item: { c: 'd' }
+    }, function (err, data) {
+        if (err) {
+            //handle error
+        } else {
+            //your logic goes here
+        }
+    });
+    s3.copyObject({
+        'Bucket': "dynamo-lambda-redshift-data",
+        'CopySource': "/apig-kine-proxy/key",
+        'Key': "key"
+    }).promise()
+        .then(data => {
+            console.log(data);           // successful response
+            /*
+            data = {
+                CopyObjectResult: {
+                    ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
+                    LastModified: <Date Representation>
+                }
+            }
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
+    sqs.receiveMessage({
+        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/359675929438/a',
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '1',
+        VisibilityTimeout: '30',
+        WaitTimeSeconds: '0'
+    }, function (err, data) {
+        if (err) {
+            console.log("Receive Error", err);
+        } else if (data.Messages) {
+            var message = data.Messages[0]
+            // your logic to access this fetched message, should be here
+        } else {
+            console.log("No messages found in the queue");
+        }
+    });
 }
