2417554180939b00b30acb67905e531d094e4491
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-11 08:27:35 +0530

    3!!

---
+++ tak/tak.js
@@ -0,0 +1,18 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
+    sns.publish({
+        Message: 'd',
+        Subject: 'c',
+        MessageAttributes: {},
+        MessageStructure: 'String',
+        TopicArn: 'arn:aws:sns:us-east-1:359675929438:a'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
+}
