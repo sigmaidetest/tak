8916639bbb2948996c00b193ea8a8e70c153fca9
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-12 09:50:59 +0530

    tigma

--- tak/tak-lambda.js
+++ tak/tak-lambda.js
@@ -1,18 +1,32 @@
 let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
+const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
-    sns.publish({
-        Message: 'd',
-        Subject: 'c',
-        MessageAttributes: {},
-        MessageStructure: 'String',
-        TopicArn: 'arn:aws:sns:us-east-1:359675929438:a'
+    s3.listObjects({
+        'Bucket': 'fak',
+        'MaxKeys': 10,
+        'Prefix': '5'
     }).promise()
         .then(data => {
-            // your code goes here
+            console.log(data);           // successful response
+            /*
+            data = {
+             Contents: [
+                {
+                   ETag: "\\"70ee1738b6b21e2c8a43f3a5ab0eee71\\"",
+                   Key: "example1.jpg",
+                   LastModified: <Date Representation>,
+                   Owner: {
+                      DisplayName: "myname",
+                      ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
+                   },
+                   Size: 11,
+                   StorageClass: "STANDARD"
+                },
+                {...}
+            */
         })
         .catch(err => {
-            // error handling goes here
+            console.log(err, err.stack); // an error occurred
         });
-
 }
