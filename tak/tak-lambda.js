38f883e44bb5b4ed09d53c2ac857d102a8850507
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-17 18:49:13 +0530

    tok

--- tak/tak-lambda.js
+++ tak/tak-lambda.js
@@ -1,17 +1,6 @@
 let AWS = require('aws-sdk');
-const kinesis = new AWS.Kinesis();
-exports.handler = function (event, context, callback) {
-
-	kinesis.describeStream({
-		StreamName: 'mika'
-	}).promise()
-		.then(describeStreamData => {
-			// your logic goes here
-		})
-		.catch(err => {
-			// error handling goes here
-		});
+exports.handler = function(event, context, callback) {
 
 
-	callback(null, 'Successfully executed');
+	callback(null,'Successfully executed');
 }
