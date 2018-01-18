66b595c68d0efaa81c8380f7a50862c7bac613db
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-18 14:04:42 +0530

    topi

--- tak/tak-lambda.js
+++ tak/tak-lambda.js
@@ -1,22 +1,6 @@
 let AWS = require('aws-sdk');
-let SL = require('@slappforge/slappforge-sdk');
-const sqs = new SL.AWS.SQS(AWS);
-exports.handler = function (event, context, callback) {
-	sqs.receiveMessage({
-		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/359675929438/sqssa',
-		AttributeNames: ['All'],
-		MaxNumberOfMessages: '1',
-		VisibilityTimeout: '30',
-		WaitTimeSeconds: '0'
-	}, function (receivedMessages) {
-		receivedMessages.forEach(message => {
-			// your logic to access each message through out the loop. Each message is available under variable message 
-			// within this block
-		})
-	}, function (error) {
-		// implement error handling logic here
-	});
+exports.handler = function(event, context, callback) {
 
 
-	callback(null, 'Successfully executed');
+	callback(null,'Successfully executed');
 }
