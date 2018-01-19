f9dfc51d64b8d037eb0483738a31d6b8d1e46bb6
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-19 15:17:08 +0530

    fafa

--- tak/tak-lambda.js
+++ tak/tak-lambda.js
@@ -1,9 +1,8 @@
 let AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
-
 	s3.listObjects({
-		'Bucket': 'dynamo-lambda-redshift-data',
+		'Bucket': 'panittuwak',
 		'MaxKeys': 10,
 		'Prefix': ''
 	}).promise()
@@ -24,6 +23,25 @@ exports.handler = function (event, context, callback) {
 				   StorageClass: "STANDARD"
 				},
 				{...}
+			*/
+		})
+		.catch(err => {
+			console.log(err, err.stack); // an error occurred
+		});
+
+	s3.putObject({
+		"Body": "a",
+		"Bucket": "panittuwak",
+		"Key": "b"
+	})
+		.promise()
+		.then(data => {
+			console.log(data);           // successful response
+			/*
+			data = {
+				ETag: "\\"6805f2cfc46c0f04559748bb039d69ae\\"", 
+				VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
+			}
 			*/
 		})
 		.catch(err => {
